05fa34c6a1b3eb47dcc8357ab2e809a8f6251dea
zachRadack <radack.zach@gmail.com>
2023-10-13 15:20:38 -0700

    added in a working login page with placeholder login system
    only utilizes a generic placeholder login system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SplashPageComponent } from './splash-page/splash-page.component';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
     { path:"", component: SplashPageComponent},
     { path:"Login", component: LoginComponent},
-    { path:"Signin", component: SigninComponent}
+    { path:"Signin", component: SigninComponent},
+    { path:"Dashboard", component: DashboardComponent}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import {FormGroup, FormControl} from '@angular/forms';
+import { LoginService } from '../login.service';
+import { style } from '@angular/animations';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    msg: string = "";
+    loginRef = new FormGroup({
+        emailid: new FormControl(),
+        password: new FormControl()
+    });
+    constructor(public loginService: LoginService, public router: Router) {}
 
+    LoginButton(): void{
+        let login = this.loginRef.value;
+        if(this.loginService.checkLoginDetails(login)){
+            this.router.navigate(["Dashboard"],{skipLocationChange: true});
+        }else{
+            this.msg = "Invalid login details";
+        }
+        this.loginRef.reset();
+
+    }
 }
